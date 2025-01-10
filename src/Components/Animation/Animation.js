ae47b5df291dadfe6bc9babf46e953ed626d0e49
Bharath Sindhe <sindhebharath10@gmail.com>
2025-01-10 17:47:04 +0530

    updated loader

--- src/Components/Animation/Animation.js
+++ src/Components/Animation/Animation.js
@@ -1,24 +1,18 @@
-import { Audio } from "react-loader-spinner";
+import "./Animation.css";
 export default function Animation() {
-  return (
-    <div style={styles.container}>
-      <Audio
-        height="80"
-        width="80"
-        radius="9"
-        color="green"
-        ariaLabel="loading"
-      />
-    </div>
-  );
-}
-
-const styles = {
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh", // Full viewport height
-    backgroundColor: "#f0f0f0", // Optional background color
-  },
-};
+    return (
+      <div style={styles.container}>
+        <div className="loader"></div>
+      </div>
+    );
+  }
+  
+  const styles = {
+    container: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh", // Full viewport height
+      backgroundColor: "#f0f0f0", // Optional background color
+    },
+  };
